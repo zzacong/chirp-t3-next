28fe5da25a2bd504342924f2941b253508d8c529
zzacong <61817066+zzacong@users.noreply.github.com>
2023-04-05 18:51:44 +1000

    setup clerk auth

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { type NextRequest } from 'next/server';
+import { withClerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export default withClerkMiddleware((_req: NextRequest) => {
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    '/((?!static|.*\\..*|_next|favicon.ico).*)',
+    '/',
+  ],
+};
