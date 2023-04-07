f464ca504e6f9adac0ca15111d10414b56144600
Zac Ong <61817066+zzacong@users.noreply.github.com>
2023-04-07 16:58:06 +1000

    create base post and profile page

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from 'next';
+import Head from 'next/head';
+
+const ProfilePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Profile | Chirp T3</title>
+      </Head>
+
+      <div className="flex flex-1 justify-center bg-gradient-to-b from-white to-zinc-100 px-6 dark:from-zinc-950 dark:to-zinc-950">
+        <main className="relative w-full border-x border-zinc-400 md:max-w-3xl"></main>
+      </div>
+    </>
+  );
+};
+
+export default ProfilePage;
