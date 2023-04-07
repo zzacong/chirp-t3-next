743184bba623da8e21880f1d8ad7f9b9b7796eda
Zac Ong <61817066+zzacong@users.noreply.github.com>
2023-04-07 22:46:04 +1000

    try edge api

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,27 @@
-import { createNextApiHandler } from '@trpc/server/adapters/next';
-
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { type NextRequest } from 'next/server';
 import { env } from '~/env.mjs';
 import { createTRPCContext } from '~/server/api/trpc';
 import { appRouter } from '~/server/api/root';
 
+export const config = {
+  runtime: 'edge',
+};
+
 // export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === 'development'
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`
-          );
-        }
-      : undefined,
-});
+export default async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: '/api/trpc',
+    router: appRouter,
+    req,
+    createContext: createTRPCContext,
+    onError:
+      env.NODE_ENV === 'development'
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`
+            );
+          }
+        : undefined,
+  });
+}
