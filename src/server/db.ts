344c94c1c5fee5a1ce493a55594be37dc2d97544
Zac Ong <61817066+zzacong@users.noreply.github.com>
2023-04-10 21:24:19 +1000

    use planetscale database js

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
+import { connect } from '@planetscale/database';
 import { env } from '~/env.mjs';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
-
-if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const db = connect({ url: env.DATABASE_URL });
--- src/server/utils.ts
+++ src/server/utils.ts
@@ -2,7 +2,7 @@ import { type User } from '@clerk/nextjs/dist/api';
 import { createProxySSGHelpers } from '@trpc/react-query/ssg';
 import superjson from 'superjson';
 import { appRouter } from '~/server/api/root';
-import { prisma } from '~/server/db';
+import { db } from '~/server/db';
 
 export const filterUserForClient = <const T extends User>(user: T) => {
   return {
@@ -15,6 +15,9 @@ export const filterUserForClient = <const T extends User>(user: T) => {
 export const generateSSGHelper = () =>
   createProxySSGHelpers({
     router: appRouter,
-    ctx: { prisma, userId: null },
+    ctx: { db, userId: null },
     transformer: superjson, // optional - adds superjson serialization
   });
+
+export const fakeCuid = () =>
+  crypto.randomUUID().replaceAll('-', '').slice(0, 25);
