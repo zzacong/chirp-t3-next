3f631c22a44dd74eec17385b468d97f2756c7006
zzacong <61817066+zzacong@users.noreply.github.com>
2023-05-13 17:04:27 +1000

    with t3-oss/env

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
-/** @type {import("tailwindcss").Config} */
-const config = {
+import { type Config } from 'tailwindcss';
+
+export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [require('tailwind-scrollbar'), require('@tailwindcss/forms')],
-};
-
-export default config;
+} satisfies Config;
