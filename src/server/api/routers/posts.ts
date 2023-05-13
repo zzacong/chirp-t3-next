74f585ee7ba984d923921c3cfcd2659f6afbfb4e
zzacong <61817066+zzacong@users.noreply.github.com>
2023-05-13 16:10:37 +1000

    change rate limit prefix

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -44,12 +44,7 @@ const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(3, '1 m'),
   analytics: true,
-  /**
-   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
-   * instance with other applications and want to avoid key collisions. The default prefix is
-   * "@upstash/ratelimit"
-   */
-  prefix: '@upstash/ratelimit',
+  prefix: 'chirp-t3',
 });
 
 export const postsRouter = createTRPCRouter({
